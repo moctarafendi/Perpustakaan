d8e9c7e0deb6b45550de44893909617ac027b663
moctarafendi <moctarafendi@gmail.com>
2023-01-04 15:48:50 +0700

    Belajar Membuat Project Perpustakaan - Create Empty Project - Create Model 	- Book 	- Category 	- Order 	- Users - Create Context Class 	- Add Model -> PustakaDbContext 	- import using Microsoft.EntityFrameworkCore - Create connection string ke database di appsettings.json - Setting startUp v1 - Create Database (PerpustakaanDB) menggunakan fitur migrate - Create DbInitalizer 	- Add Model -> DbInitalizer - Create Seed 	- Masukkan ke main di Program.cs - Create Manager and Interface - Create wwwroot folder - Install bootstrap dan jquery via libMan -> klik kanan pada libman.json lalu pilih "" Enable Restore Client-Side Libraries on Build"" - setting startUp v2 - Create _viewImport, _viewStart - Add custom css to wwwroot -> content - create ViewModel -> loginVM.css -> class data khusus untuk view - create controller login - create View Login
    =======================================================================================================================================================
    =======================================================================================================================================================
    =======================================================================================================================================================
    =======================================================================================================================================================
    =======================================================================================================================================================
    Belajar Membuat Project Perpustakaan Part 2 :
    - create _Layout
    - create shared view menu
    - add images
    - add authorization on home controller
    - add fitur management category via fitur Scaffolding
    	- add authorize only for Admin -> [Authorize(Roles = ""Admin"")]
    - add fitur management user via fitur Scaffolding
    	- add authorize only for Admin -> [Authorize(Roles = ""Admin"")]
    - add fitur management buku via fitur Scaffolding
    	- add authorize only for Admin -> [Authorize(Roles = ""Admin,Staff"")]
    - create fungsi log off
    - create fungsi daftar buku / _GetBuku
    - create view daftar buku
    - create fungsi add to cart
    	- dipanggil melalui JS
    - create session extension helper untuk menyimpan cart disession
    - add set/get cart session
    	- pada view _Menu
    - create view my order / chart
    - create function cancel order
    - create function save order
    - create ViewModel -> HistoryVM -> class data khusus untuk view
    - create fungsi dan halaman history peminjaman buku
    - create fungsi dan halaman detail buku ketika di klik di halaman daftar buku & ketika di klik di halaman history peminjaman buku
    - create fungsi dan halaman pengembalian buku
    	- create ViewModel -> PeminjamanVM.css -> class data khusus untuk view
    	- create fungsi dan halaman daftar peminjam buku
    	- create fungsi dan halaman detail peminjaman buku
    - create fungsi dan halaman ganti password
    	- create ViewModel -> ChangePasswordVM
    		"

---
+++ Perpustakaan/Controllers/LoginController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Perpustakaan.Models;
+using Perpustakaan.ViewModels;
+
+namespace Perpustakaan.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly PustakaDbContext _context;
+
+        public LoginController(PustakaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginVM model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = await _context.Users.FindAsync(model.Username);
+                    if (user != null)
+                    {
+                        if (user.Password == model.Password)
+                        {
+                            var claims = new[]
+                            {
+                                new Claim(ClaimTypes.Name, user.Username),
+                                new Claim(ClaimTypes.Role, user.Role)
+                            };
+
+                            var claimIdentities = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                            var claimPrincipal = new ClaimsPrincipal(claimIdentities);
+                            var authenticationManager = Request.HttpContext;
+
+                            // Sign In
+                            await authenticationManager.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimPrincipal, new AuthenticationProperties() { IsPersistent = false });
+                            return RedirectToAction("Index", "Home");
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Password Salah!");
+                            return View(model);
+                        }
+                    }
+
+                    ModelState.AddModelError("", "Username Salah!");
+                    return View(model);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> LogOff()
+        {
+            HttpContext.Session.Clear();
+            var authenticationManager = Request.HttpContext;
+            await authenticationManager.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Login");
+        }
+
+        [Authorize]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePassword(ChangePasswordVM model)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+
+                    var curUsername = User.Identity.Name;
+                    var user = await _context.Users.FirstOrDefaultAsync(u =>
+                        u.Username.ToLower().Equals(curUsername.ToLower()));
+                    if (user != null)
+                    {
+                        if (user.Password == model.OldPassword)
+                        {
+                            user.Password = model.NewPassword;
+                            await _context.SaveChangesAsync();
+                            ModelState.AddModelError("", "Password berhasil diganti.");
+
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Password lama salah.");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Username tidak dikenal.");
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            return View();
+        }
+
+    }
+}
